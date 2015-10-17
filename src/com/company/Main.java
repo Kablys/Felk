a9cd867bf7f6fb430600f6f365b282a16e5b6735
Kablys <mfrohd@gmail.com>
2015-10-17 12:34:23 +0300

    testing 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(args.length);
 	// write your code here
 //        just testing
     }

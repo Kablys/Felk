c1d34533068687695781b05879a94f601831db87
Kablys <mfrohd@gmail.com>
2015-10-17 12:59:13 +0300

    Add gitignore, test.felk

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,32 @@
 package com.company;
 
+import java.io.BufferedReader;
+//import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println(args.length);
-	// write your code here
-//        just testing
+    public static void main(String[] args) throws IOException {
+        if (args.length == 1){
+            //System.out.println(args[0]);
+            BufferedReader br = new BufferedReader(new FileReader(args[0]));
+            try {
+                StringBuilder sb = new StringBuilder();
+                String line = br.readLine();
+
+                while (line != null) {
+                    sb.append(line);
+                    sb.append(System.lineSeparator());
+                    line = br.readLine();
+                }
+                String everything = sb.toString();
+                System.out.println(everything);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                br.close();
+            }
+        }
     }
 }

7624b0ce1b6f25ad26779896a40e29ab41ddec3c
Kablys <mfrohd@gmail.com>
2015-11-08 16:56:09 +0200

    Node begin

---
+++ src/com/company/Node.java
@@ -0,0 +1,8 @@
+package com.company;
+
+import java.util.List;
+
+public class Node{
+    private Lexer.Token token;
+    private List<Node> children;
+}
--- src/com/company/Parser.java
+++ src/com/company/Parser.java
@@ -2,14 +2,42 @@ package com.company;
 
 import java.util.List;
 
-/**
- * Created by domas on 15.11.8.
- */
 public class Parser {
-    public Parser(List<Lexer.Token> tokens) {
+    static Node ast;
+    static Integer nTok = 0;
+    static List<Lexer.Token> tokens;
+    public Parser(List<Lexer.Token> ltokens) {
+        tokens = ltokens;
         for(Lexer.Token t : tokens) {
             System.out.println(t);
         }
+        ast = programParse(getNextToken());
+    }
+
+    public static Lexer.Token getNextToken () {
+
+        if (nTok < tokens.size()) {
+            Lexer.Token t = tokens.get(nTok);
+            nTok++;
+            return t;
+        } else {
+            nTok = -1;
+            return tokens.get(tokens.size() - 1);
+        }
+    }
+
+
+    public Node programParse (Lexer.Token token){
+        Node node;
+        if (token.t == Lexeme.MAIN)
+            mainParse(getNextToken());
+        return null;
+    }
+    public Node mainParse (Lexer.Token token){
+        if (token.t == Lexeme.MAIN)
+
+            return null;
+        return null;
     }
 
 }

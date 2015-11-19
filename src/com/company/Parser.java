4ac0a40c87053ec7fefd12d75d1d2bfb568f9020
Lukas <lukas.karmanovas@mif.stud.vu.lt>
2015-11-19 18:27:26 +0200

    Kazka bandziau padaryti su expression, nepaejo

--- src/com/company/Parser.java
+++ src/com/company/Parser.java
@@ -9,8 +9,10 @@ public class Parser {
     String output = "";
     static Node ast;
     static Integer nTok = 0;
+    int skip = 0;
     static List<Lexer.Token> tokens;
     //static Lexeme[] arrayOfTypes = {com.company.Lexeme.INT, com.company.Lexeme.FLOAT, com.company.Lexeme.STRING, com.company.Lexeme.CHAR, com.company.Lexeme.BOOL, com.company.Lexeme.VOID};
+
     List<Lexeme> arrayOfTypes = Arrays.asList(com.company.Lexeme.INT, com.company.Lexeme.FLOAT, com.company.Lexeme.STRING,
             com.company.Lexeme.CHAR, com.company.Lexeme.BOOL, com.company.Lexeme.VOID);
     List<Lexeme> relatOp = Arrays.asList(com.company.Lexeme.EQUAL, com.company.Lexeme.LESS, com.company.Lexeme.MORE,
@@ -18,6 +20,7 @@ public class Parser {
     List<Lexeme> addOp = Arrays.asList(com.company.Lexeme.PLUS, com.company.Lexeme.MINUS, com.company.Lexeme.OROP);
     List<Lexeme> mulOp = Arrays.asList(com.company.Lexeme.MULTIPLY, com.company.Lexeme.DIVISION, com.company.Lexeme.MOD,
             com.company.Lexeme.ANDOP);
+
     //myList = {Lexeme.INT, Lexeme.FLOAT, Lexeme.STRING, Lexeme.CHAR, Lexeme.BOOL, Lexeme.VOID};
     public Parser(List<Lexer.Token> programsTokens) {
         tokens = programsTokens;
@@ -25,7 +28,7 @@ public class Parser {
         for(Lexer.Token t : tokens) {
             System.out.println(i++ +" "+t);
         }
-        ast = programParse(0);
+        ast = programParse(36);
         output = ast.toXml(0);
         ast.toXml2(output);
     }
@@ -62,24 +65,13 @@ public class Parser {
 
     public Node functionParse (Integer index){
         Node node = new Node (new Lexer.Token(Lexeme.FUNCTION, "<function>"));
-        node.addChildren(typeParse(index - 1));
-        index++;
-        node.addChildren(paramParse(index));
-        index = nTok;
-        node.addChildren(blockParse(index+2));
-
-//        node.addChildren(new Node (tokens.get(index - 1)));
-//        if (tokens.get(index).t == Lexeme.IDENTIFIER){
-//            node.addChildren(new Node (tokens.get(index)));
-//            node.addChildren(paramParse(index + 1));
-//        }
+        node.addChildren(new Node (tokens.get(index - 1)));
+        if (tokens.get(index).t == Lexeme.IDENTIFIER){
+            node.addChildren(new Node (tokens.get(index)));
+            node.addChildren(paramParse(index + 1));
+        }
         return node;
-//        if (token.t == Lexeme.MAIN)
-//
-//            return null;
-//        return null;
     }
-
 
     public Node paramParse (Integer index){
         Node node = new Node (new Lexer.Token(Lexeme.PARAMETER, "test"));
@@ -87,13 +79,9 @@ public class Parser {
             index++;
             while (tokens.get(index).t != Lexeme.RPAREN) {
                 node.addChildren(typeParse(index));
-                index = index + 2;
-                if (tokens.get(index).t == Lexeme.COMMA) {
-                    index++;
-                }
+                index++;
             }
         }
-        nTok = index;
         return node;
     }
 
@@ -115,7 +103,10 @@ public class Parser {
                 //System.out.println("Found comma at " + index);
                 index++;
             }
-
+            if((relatOp.contains(tokens.get(index).t))||(mulOp.contains(tokens.get(index).t))||(addOp.contains(tokens.get(index).t))){
+                node.addChildren(expressionParse(index));
+                index++;
+            }
         }
         //else{}
         return node;
@@ -125,10 +116,11 @@ public class Parser {
         if (tokens.get(index+2).t == Lexeme.ASSIG){
             index = index+2;
             Node node = new Node(tokens.get(index));
-            Node nodeInt = new Node(tokens.get(index-2));
+            Node nodeInt = new Node(tokens.get(index - 2));
             node.addChildren(nodeInt);
-            nodeInt.addChildren(new Node(tokens.get(index-1)));
-            node.addChildren(new Node(new Lexer.Token(Lexeme.EXPRESSION,"Expresion")));
+            nodeInt.addChildren(new Node(tokens.get(index - 1)));
+            node.addChildren(expressionParse(index));
+
             return node;
         }else{
             Node node = new Node(tokens.get(index));
@@ -136,6 +128,11 @@ public class Parser {
             node.addChildren(new Node(tokens.get(index)));
             return node;
         }
+    }
 
+    public Node expressionParse (int index) {
+        Node node = new Node(new Lexer.Token(Lexeme.EXPRESSION,"Expression"));
+        if(tokens.get(index) simpleExpression)
+        return node;
     }
 }
